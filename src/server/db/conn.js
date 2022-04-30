ccc6b73127b630604045fe9781e8badfd196f39e
rina-karnauch <rina.karnauch@mail.huji.ac.il>
2022-05-01 01:58:09 +0300

    mongo security

--- src/server/db/conn.js
+++ src/server/db/conn.js
@@ -13,7 +13,7 @@ module.exports = {
             // Verify we got a good "db" object
             if (db)
             {
-                _db = db.db("hujiConf");
+                _db = db.db("HEHEHEHEHEHEHEEEEEEEE");
                 console.log("Successfully connected to MongoDB.");
             }
             return callback(err);
--- src/server/routes/record.js
+++ src/server/routes/record.js
@@ -14,7 +14,7 @@ const ObjectId = require("mongodb").ObjectId;
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
-    let db_connect = dbo.getDb("hujiConf");
+    let db_connect = dbo.getDb("HEHEHEHEHEHEHEEEEEEEE");
     db_connect
         .collection("records")
         .find({})
